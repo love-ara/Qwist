93e180e9a55e85b9f67c69c9dbd7837c3a758ffc
Aramide <aramideotenaike@gmail.com>
2025-04-11 05:46:10 +0100

    encryption service

---
+++ src/main/java/africa/semicolon/service/impl/EncryptorServiceImpl.java
@@ -0,0 +1,15 @@
+package africa.semicolon.service.impl;
+
+import africa.semicolon.service.services.EncryptorService;
+
+public class EncryptorServiceImpl implements EncryptorService {
+    @Override
+    public String encryptData(Object data) {
+        return null;
+    }
+
+    @Override
+    public String decryptData(Object data) {
+        return null;
+    }
+}
---
+++ src/main/java/africa/semicolon/service/services/EncryptorService.java
@@ -0,0 +1,6 @@
+package africa.semicolon.service.services;
+
+public interface EncryptorService {
+    String encryptData(Object data);
+    String decryptData(Object data);
+}
